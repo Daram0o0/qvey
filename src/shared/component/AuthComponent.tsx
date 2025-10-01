e72cdd26cc7ae0e957eb59c89fe8b5dfe2384ccf
ptk5758 <ptk725739@gmail.com>
2025-10-01 22:38:57 +0900

    [Descript] 코드 정리

--- src/shared/component/AuthComponent.tsx
+++ src/shared/component/AuthComponent.tsx
@@ -1,24 +1,12 @@
 import { AuthContext } from '@/app/context'
-import { useContext, useEffect, type PropsWithChildren } from 'react'
-import { useNavigate } from 'react-router'
+import { useContext, type PropsWithChildren } from 'react'
 
 /**
  * 로그인 되어 있는 사용자만 접근 가능한 컴포넌트
  */
 function AuthComponent({ children }: PropsWithChildren) {
     const authContext = useContext(AuthContext)
-    const navigate = useNavigate()
-    // useEffect(() => {
-    //     if (!authContext) {
-    //         throw new Error('Auth Context null error')
-    //     }
-    //     const { accessToken } = authContext
 
-    //     if (!accessToken) {
-    //         alert('로그인 하세요')
-    //         navigate('/')
-    //     }
-    // }, [])
     if (!authContext) {
         return <div>AuthContext error</div>
     }
