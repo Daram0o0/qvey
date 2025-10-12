4375c0042692e4971cdb980c6c02b2bbb253ee59
Daram0o0 <c30728535@gmail.com>
2025-10-12 15:25:47 +0900

    [Refector] 'Card.Footer' 에 데이터 렌더링 책임 위임
    1. CardData 타입 구조화
        - CardData 타입을 구조화하여 thumbnail, content, footer로 분리
        - thumbnail: thumbnailUrl, isBookmarked
        - content: title, description, likes
        - footer: createdAt, user
    2. Card.Footer 책임 변경
        - 기존 : Card.Footer 컴포넌트는 단순한 스타일 wrapper 역할만을 담당
        - 변경 : Card.Footer 컴포넌트가 footer 데이터를 받아 렌더링 책임을 담당
    3. DEMO_CardItemPage 수정
        - DEMO_CardItemPage에서 CardData 타입에 맞게 데이터 구조 변경

--- src/shared/card/ui/Card.tsx
+++ src/shared/card/ui/Card.tsx
@@ -1,8 +1,14 @@
+import { FaUserCircle } from 'react-icons/fa'
 import styles from './Card.module.css'
+import type { footerType } from '@/widgets/cardItem/ui/CardItem'
 
 export type CardProps = {
-    children: React.ReactNode
+    children?: React.ReactNode
     className?: string
+}
+
+export type CardFooterProps = CardProps & {
+    footer: footerType
 }
 
 function Card({ children, className = '' }: CardProps) {
@@ -17,8 +23,16 @@ Card.Content = function ({ children, className = '' }: CardProps) {
     return <div className={`${styles.content} ${className}`}>{children}</div>
 }
 
-Card.Footer = function ({ children, className = '' }: CardProps) {
-    return <footer className={`${styles.footer} ${className}`}>{children}</footer>
+Card.Footer = function ({ footer, className = '' }: CardFooterProps) {
+    return (
+        <footer className={`${styles.footer} ${className}`}>
+            <p className={styles.date}>{footer.createdAt}</p>
+            <div className={styles.userInfo}>
+                <FaUserCircle />
+                <span>{footer.user.name}</span>
+            </div>
+        </footer>
+    )
 }
 
 export default Card
--- src/widgets/cardItem/ui/CardItem.tsx
+++ src/widgets/cardItem/ui/CardItem.tsx
@@ -1,22 +1,31 @@
 import Card from '@/shared/card/ui/Card'
 import { FaBookmark } from 'react-icons/fa6'
-import { FaUserCircle } from 'react-icons/fa'
 import { PiHeartDuotone } from 'react-icons/pi'
 import reactLogo from '@/assets/react.svg'
 import styles from './CardItem.module.css'
 
-export type CardData = {
-    id: number
+export type thumbnailType = {
+    thumbnailUrl?: string
+    isBookmarked: boolean
+}
+export type contentType = {
     title: string
     description: string
-    thumbnailUrl?: string
+    likes?: number
+}
+export type footerType = {
     createdAt: string
     user: {
         name: string
         avatarUrl?: string
     }
-    likes: number
-    isBookmarked?: boolean
+}
+
+export type CardData = {
+    id: number
+    thumbnail?: thumbnailType
+    content: contentType
+    footer?: footerType
 }
 
 export type CardItemProps = {
@@ -26,30 +35,26 @@ export type CardItemProps = {
 function CardItem({ card }: CardItemProps) {
     return (
         <Card>
-            {card.thumbnailUrl && (
+            {card.thumbnail && (
                 <Card.Thumbnail>
-                    <img src={card.thumbnailUrl || reactLogo} alt="문제집 썸네일" className={styles.thumbnailImage} />
+                    <img
+                        src={card.thumbnail.thumbnailUrl || reactLogo}
+                        alt="문제집 썸네일"
+                        className={styles.thumbnailImage}
+                    />
                     <FaBookmark className={styles.bookmark} />
                 </Card.Thumbnail>
             )}
 
             <Card.Content>
                 <div className={styles.title}>
-                    <h2>{card.title}</h2>
-                    {card.thumbnailUrl && <PiHeartDuotone />}
+                    <h2>{card.content.title}</h2>
+                    {card.content.likes && <PiHeartDuotone />}
                 </div>
-                <p className={styles.description}>{card.description}</p>
+                <p className={styles.description}>{card.content.description}</p>
             </Card.Content>
 
-            {card.thumbnailUrl && (
-                <Card.Footer>
-                    <p className={styles.date}>{card.createdAt}</p>
-                    <div className={styles.userInfo}>
-                        <FaUserCircle />
-                        <span>{card.user.name}</span>
-                    </div>
-                </Card.Footer>
-            )}
+            {card.footer && <Card.Footer footer={card.footer} />}
         </Card>
     )
 }
